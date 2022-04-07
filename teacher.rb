35812c86252b2fcd41e99c48f3b5c4202775ae05
mugisha Samuel <mugishasamuel42@gmail.com>
2022-04-07 08:22:29 -0700

    Add associations between rentals,books and person

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require('./person')
 
 class Teacher < Person
-  def initialize(specialization)
-    super(20, 'fabrice', true)
+  def initialize(specialization,age, name = 'unknown', parent_permission: true)
+    super(age, name = 'unknown', parent_permission: true)
     @specialization = specialization
   end
 
